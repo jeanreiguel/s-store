e0973371da36abdc439611ee6a45faa5df2f68e3
jenasw <jeanrgl03@gmail.com>
2021-05-07 10:48:29 -0300

    refatorações

--- src/br/com/senai/view/MainProgram.java
+++ src/br/com/senai/view/MainProgram.java
@@ -3,7 +3,10 @@ package br.com.senai.view;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.senai.controller.ProdutoController;
+import br.com.senai.controller.Controller;
+import br.com.senai.controller.carrinho.*;
+import br.com.senai.controller.produto.*;
+import br.com.senai.controller.cliente.*;
 import br.com.senai.model.CarrinhoModel;
 import br.com.senai.model.ProdutoModel;
 
@@ -11,32 +14,45 @@ public class MainProgram {
 	public static void main(String[] args) {
 		List<ProdutoModel> produtos = new ArrayList<ProdutoModel>();
 		List<CarrinhoModel> itensNoCarrinho = new ArrayList<CarrinhoModel>();
-		ProdutoController produtoController = new ProdutoController();
-
+		
+		Controller Controller = new Controller();
+		ListaCarrinho listaCarrinho = new ListaCarrinho();
+		AdicionarCarrinho AdicionarCarrinho = new AdicionarCarrinho();
+		CadastrarProduto CadastrarProduto = new CadastrarProduto();
+		ListaProduto ListaProduto = new ListaProduto();
+		EditarProduto EditarProduto = new EditarProduto();
+		DeletarProduto DeletarProduto = new DeletarProduto();
+		DefinirCliente DefinirCliente = new DefinirCliente();
+		
 		boolean sair = false;
-
+		
+		String cliente = DefinirCliente.definirCliente();
+		
 		do {
-			produtoController.menu();
-			int opc = produtoController.opcao();
-
+			Controller.menu();
+			int opc = Controller.opcao();
+		
 			switch (opc) {
 			case 1:
-				produtos.add(produtoController.cadastrarProduto());
+				produtos.add(CadastrarProduto.cadastrarProduto());
 				break;
 			case 2:
-				produtoController.consultarProdutos(produtos);
+				ListaProduto.consultarProdutos(produtos);
 				break;
 			case 3:
-				produtoController.editarProduto(produtos);
+				EditarProduto.editarProduto(produtos);
 				break;
 			case 4:
-				produtoController.removerProdutos(produtos);
+				DeletarProduto.removerProdutos(produtos);
 				break;
 			case 5:
-				itensNoCarrinho.add(produtoController.cadastrarItemCarrinho(produtos));
+				itensNoCarrinho.add(AdicionarCarrinho.cadastrarItemCarrinho(produtos));
 				break;
 			case 6:
-				produtoController.listarItensNoCarrinho(itensNoCarrinho);
+				listaCarrinho.listarItensNoCarrinho(itensNoCarrinho);
+				break;
+			case 7:
+				listaCarrinho.gerarCupom(itensNoCarrinho,cliente);
 				break;
 			case 9:
 				sair = true;
