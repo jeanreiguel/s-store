802fcb36d95a5d990867697dd26e13e1b278e2dc
jenasw <jeanrgl03@gmail.com>
2021-05-11 16:51:22 -0300

    Listagem de dados do banco

--- src/br/com/senai/controller/produto/ListaProduto.java
+++ src/br/com/senai/controller/produto/ListaProduto.java
@@ -1,18 +1,48 @@
 package br.com.senai.controller.produto;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-import br.com.senai.model.ProdutoModel;
+
+import br.com.dal.DatabaseConnection;
 
 public class ListaProduto {
-	public List<ProdutoModel> consultarProdutos(List<ProdutoModel> produtos) {
-
-		if (produtos.size() <= 0) {
-			System.out.println("Não há produtos para serem listados");
+	
+	private Connection connection;
+	
+	public ListaProduto() {
+		connection = DatabaseConnection.getInstance().getConnection();
+	}
+	
+	public ResultSet consultarProdutos() {
+		PreparedStatement preparedStatement;
+		
+		try {
+			preparedStatement = connection.prepareStatement("SELECT * FROM produto");
+			ResultSet resultset = preparedStatement.executeQuery();
+			System.out.println("\n----- PRODUTOS CADASTRASDOS -----\n");
+			System.out.printf("| %2s | %15s | %8s | %4s | %9s2f |\n", "ID", "Produto", "Preço", "Qtd", "R$ total");
+			while(resultset.next()) {
+				System.out.printf("| %2s | %15s | %8s | %4s | %9s2f |\n",
+								   resultset.getInt("codigo"),
+								   resultset.getString("nomeDoProduto"),
+								   resultset.getDouble("precoDoProduo"),
+								   resultset.getInt("quantidade"),
+								   resultset.getDouble("saldoEmEstoque"));
+			}
+			
+			return resultset;
+		} catch (Exception e){
 			return null;
 		}
-		System.out.println("\n----- PRODUTOS CADASTRASDOS -----\n");
-		System.out.printf("| %2s | %10s | %8s | %4s | %9s2f |\n", "ID", "Produto", "Preço", "Qtd", "R$ total");
+
+		//if (produtos.size() <= 0) {
+		//	System.out.println("Não há produtos para serem listados");
+		//	return null;
+		//}
+		//System.out.println("\n----- PRODUTOS CADASTRASDOS -----\n");
+		//System.out.printf("| %2s | %10s | %8s | %4s | %9s2f |\n", "ID", "Produto", "Preço", "Qtd", "R$ total");
 		// for (ProdutoModel produtoModel : produtos) {
 		// System.out.printf("| %10s | %8s | %4s | %9s |\n",
 		// produtoModel.getNomdeDoProduto(),
@@ -29,12 +59,10 @@ public class ListaProduto {
 		// produto.getSaldoEmEstoque());
 		// });
 		// return produtos;
-		for (int i = 0; i < produtos.size(); i++) {
-			System.out.printf("| %2s | %10s | %8s | %4s | %9s |\n", i + 1, produtos.get(i).getNomeDoProduto(),
-					produtos.get(i).getPrecoDoProduto(), produtos.get(i).getQuantidadeDeProduto(),
-					produtos.get(i).getSaldoEmEstoque());
-		}
-
-		return produtos;
+		//for (int i = 0; i < produtos.size(); i++) {
+		//	System.out.printf("| %2s | %10s | %8s | %4s | %9s |\n", i + 1, produtos.get(i).getNomeDoProduto(),
+		//			produtos.get(i).getPrecoDoProduto(), produtos.get(i).getQuantidadeDeProduto(),
+		//			produtos.get(i).getSaldoEmEstoque());
+		//}
 	}
 }
