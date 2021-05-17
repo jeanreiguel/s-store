df18d43db74d0b6139ffd2db4eed54400c2e0554
jenasw <jeanrgl03@gmail.com>
2021-05-17 11:41:42 -0300

    Refatoração para compatibilidade com BD

--- src/br/com/senai/controller/carrinho/AdicionarCarrinho.java
+++ src/br/com/senai/controller/carrinho/AdicionarCarrinho.java
@@ -1,55 +1,89 @@
 package br.com.senai.controller.carrinho;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
-import br.com.senai.model.CarrinhoModel;
 import br.com.senai.model.ProdutoModel;
-import br.com.senai.controller.produto.EditarProduto;
+import br.com.dal.DatabaseConnection;
+import br.com.senai.controller.produto.DeletarProduto;
 import br.com.senai.controller.produto.ListaProduto;
 
 public class AdicionarCarrinho {
-	Scanner dgt = new Scanner(System.in);
 	
-	
-	public CarrinhoModel cadastrarItemCarrinho(List<ProdutoModel> produtos) {
-		ListaProduto ListaProduto = new ListaProduto();
-		EditarProduto EditarProduto = new EditarProduto();
-		CarrinhoModel carrinhoModel = new CarrinhoModel();
+	private Connection connection;
+	private Scanner dgt = new Scanner(System.in);
+	private PreparedStatement preparedStatement;
+	public AdicionarCarrinho() {
+		connection = DatabaseConnection.getInstance().getConnection();
+	}
+	public void cadastrarItemCarrinho(int cliente) {
 		
-		if(produtos.size() <= 0) {
-			System.out.println("Não há produtos. ");
-			return null;
-		}
+		
+		ListaProduto ListaProduto = new ListaProduto();
+		ProdutoModel produto = new ProdutoModel();
+		DeletarProduto DeletarProduto = new DeletarProduto();
 		ListaProduto.consultarProdutos();
 		
 		System.out.println("--- ADICIONAR ITEM NO CARRINHO ---");
 		System.out.println("Informar o ID do produto: ");
-		carrinhoModel.setIdProduto(dgt.nextInt() - 1);
+		int idDoProduto = dgt.nextInt();
 		
-		int idProduto = (carrinhoModel.getIdProduto());
-		
-		if(idProduto > produtos.size()) {
-			System.out.println("Este produto não está cadastrado");
-			return null;
-		} 
+		if(!DeletarProduto.VerificaExistencia(idDoProduto)) {
+			return;
+		}
 		System.out.println("Informe a quantidade desejada: ");
-		carrinhoModel.setQuantidadeItens(dgt.nextInt());
+		produto.setQuantidadeDeProduto(dgt.nextInt());
 		
-		
-		if(carrinhoModel.getQuantidadeItens() > produtos.get(carrinhoModel.getIdProduto()).getQuantidadeDeProduto()) {
-			System.out.println("O produto não possui a quantidade desejada");
+		try {
+			String sql = "SELECT nomeDoProduto, precoDoProduo, quantidade, saldoEmEstoque FROM produto WHERE codigo = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, idDoProduto);
 			
+			ResultSet resultset = preparedStatement.executeQuery();
+			resultset.next();
+			if(resultset.getInt("quantidade") < produto.getQuantidadeDeProduto()) {
+				System.out.println("Quantidade em estoque insuficiente.");
+				return;
+			}
+			produto.setNomeDoProduto(resultset.getString("nomeDoProduto"));	
+			produto.setPrecoDoProduto(resultset.getDouble("precoDoProduo"));
+			produto.setSaldoEmEstoque(resultset.getDouble("saldoEmEstoque"));
+			AtualizarEstoque(idDoProduto, resultset.getInt("quantidade"), produto.getQuantidadeDeProduto());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		
-		EditarProduto.atualizarQuantidadeEValor(produtos, carrinhoModel.getQuantidadeItens(), idProduto);
-		
-		carrinhoModel.setProduto(produtos.get(idProduto));
-		carrinhoModel.setValorTotalPorItem(carrinhoModel.getQuantidadeItens() *
-				produtos.get(idProduto).getPrecoDoProduto());
-		if(produtos.get(idProduto).getQuantidadeDeProduto() == 0) {
-			produtos.remove(idProduto);
+		try {
+			String sql = "INSERT INTO carrinho (CLIENTE, ID, PRODUTO, QUANTIDADE, PRECO_UNITARIO, TOTAL_ITEM)"
+					+ "VALUES (?, ?, ?, ?, ?, ?)";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, cliente);
+			preparedStatement.setInt(2, idDoProduto);
+			preparedStatement.setString(3, produto.getNomeDoProduto());
+			preparedStatement.setInt(4,produto.getQuantidadeDeProduto());
+			preparedStatement.setDouble(5,produto.getPrecoDoProduto());
+			preparedStatement.setDouble(6, produto.getQuantidadeDeProduto() * produto.getPrecoDoProduto());
+			preparedStatement.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return carrinhoModel;
+	}
+	public void AtualizarEstoque(int id, int quantidade, int quantidadecarrinho) {
+	
+		try {
+			int quant = quantidade-quantidadecarrinho;
+			String sql = "UPDATE produto SET quantidade = ? WHERE codigo = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, quant);
+			preparedStatement.setInt(2, id);
+			preparedStatement.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
