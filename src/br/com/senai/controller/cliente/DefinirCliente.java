9a9415f26219783797377f32e4ee71304362a45b
jenasw <jeanrgl03@gmail.com>
2021-05-17 11:43:03 -0300

    Refatoração para BD e Associação com carrinho

--- src/br/com/senai/controller/cliente/DefinirCliente.java
+++ src/br/com/senai/controller/cliente/DefinirCliente.java
@@ -1,14 +1,41 @@
 package br.com.senai.controller.cliente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
+import br.com.dal.DatabaseConnection;
+
 public class DefinirCliente {
-	Scanner dgt = new Scanner(System.in);
+	private Scanner dgt = new Scanner(System.in);
+	private PreparedStatement preparedStatement;
+	private Connection connection;
 	
-	public String definirCliente() {
+	public DefinirCliente() {
+		connection = DatabaseConnection.getInstance().getConnection(); 
+	}
+	public int definirCliente() {
 		String nome;
 		System.out.println("Informe o nome do cliente");
 		nome = dgt.next();
-		return nome;
-}
+		
+		try {
+			String sql = "INSERT INTO clientes (nome) VALUES (?)";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, nome);
+			preparedStatement.execute();
+			sql = "SELECT ID FROM clientes WHERE nome = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, nome);
+			ResultSet resultset = preparedStatement.executeQuery();
+			resultset.next();
+			int codigocliente = resultset.getInt("ID");
+			return codigocliente;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
